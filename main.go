60a04fdf252b10da45f9af7f1bbed122a6818b30
Khambar Dussaliyev <anuarkaliyev23@gmail.com>
2022-10-14 02:56:01 +0600

    Добил генерацию карт

--- main.go
+++ main.go
@@ -1,27 +1,53 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"github.com/Kolesa-Education/kolesa-upgrade-homework-8/card"
+	"github.com/samber/lo"
+	"log"
 	"math/rand"
-	"time"
+	"os"
 )
 
+func cardsToRepresentations(cards []card.Card) []string {
+	representations := lo.Map[card.Card, string](cards, func(c card.Card, index int) string {
+		r, _ := c.ShortRepresentation()
+		return r
+	})
+	return representations
+}
+
 func main() {
-	seed := time.Now().UnixNano()
+	var seed int64 = 1665694295623135151
 	randomSource := rand.NewSource(seed)
 	random := rand.New(randomSource)
-	fmt.Printf("Initialized random with seed %d\n", seed)
+	log.Printf("Initialized random with seed %d\n", seed)
 
 	fmt.Println("Starting to generate cards...")
-	for i := 0; i < 100_000; i++ {
-		cardsInFile := random.Intn(7) + 3 // [3, 10)
-		cards := make([]card.Card, cardsInFile)
+	for i := 0; i < 1000; i++ {
+		log.Printf("Iteration %d\n", i)
+		cardsInFile := random.Intn(5) + 3 // [4, 7)
+		cards := make([]card.Card, 0)
 
 		for j := 0; j < cardsInFile; j++ {
 			generatedCard, _ := card.Random(*random)
 			cards = append(cards, *generatedCard)
 		}
+		log.Printf("Generated cards %s\n", cards)
+		summary := cardsToRepresentations(cards)
+		file, err := os.Create(fmt.Sprintf("dataset/dat%d.csv", i))
 
+		if err != nil {
+			log.Fatalln("failed to open file", err)
+		}
+
+		writer := csv.NewWriter(file)
+		if err = writer.Write(summary); err != nil {
+			log.Fatalln("error writing to a file!")
+		}
+
+		writer.Flush()
+		_ = file.Close()
 	}
 }
