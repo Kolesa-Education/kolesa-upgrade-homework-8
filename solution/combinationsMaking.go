192e0c4677afc46a6f5999201a877fab6005e128
Ли Владлен Александрович <vladlen.li@bas.kz>
2022-10-20 12:19:42 +0600

    Little refractoring'

--- solution/combinationsMaking.go
+++ solution/combinationsMaking.go
@@ -12,21 +12,13 @@ func makeCombinations(cardsSlice []string) string {
 	length := len(cardsSlice)
 	res := ""
 	for i := 0; i < length-1; i++ {
-		//res[i] = make([]string, length)
-
 		for j := i + 1; j < length; j++ {
 			for k := j + 1; k < length; k++ {
 				for l := k + 1; l < length; l++ {
-					cnt := 0
-					//tmp := make([]string, length)
 					for m := l + 1; m < length; m++ {
 						tmpRes := cardsSlice[i] + "," + cardsSlice[j] + "," + cardsSlice[k] + "," + cardsSlice[l] + "," + cardsSlice[m]
 						res += tmpRes + ";"
-						//tmp[cnt] = tmpRes
-						cnt++
-						//fmt.Println(res)
 					}
-					//res[i] = tmp
 				}
 			}
 		}
