77a607fae215a8b36e20fd740415a468d468570d
Nugman <bogenbayev.2001@gmail.com>
2022-10-29 11:46:16 +0600

    Nugman Bogenbayev

--- main.go
+++ main.go
@@ -4,11 +4,47 @@ import (
 	"encoding/csv"
 	"fmt"
 	"github.com/Kolesa-Education/kolesa-upgrade-homework-8/card"
+	"github.com/Kolesa-Education/kolesa-upgrade-homework-8/pockerCombination"
 	"github.com/samber/lo"
 	"log"
 	"math/rand"
 	"os"
 )
+
+func generateAll(cards []card.Card, index int, res []card.Card, id int) {
+	if index == len(cards) {
+		return
+	}
+
+	if len(res) == 5 {
+		combination := pockerCombination.Combination{
+			res,
+		}
+		if title, ok := combination.GetTitle(); ok {
+			fmt.Println("%s %s\n", res, title)
+			summary := cardsToRepresentations(res)
+			size := len(summary)
+			summary[size-1] = summary[size-1] + " | " + title
+			file, err := os.Create(fmt.Sprintf("dataset/dat%d.csv", id))
+			if err != nil {
+				log.Fatalln("failed to open file", err)
+			}
+
+			writer := csv.NewWriter(file)
+			if err = writer.Write(summary); err != nil {
+				log.Fatalln("error writing to a file!")
+			}
+			id++
+			writer.Flush()
+			_ = file.Close()
+		}
+		return
+	}
+	res = append(res, cards[index])
+	generateAll(cards, index+1, res, id)
+	res = res[:len(res)-1]
+	generateAll(cards, index+1, res, id)
+}
 
 func cardsToRepresentations(cards []card.Card) []string {
 	representations := lo.Map[card.Card, string](cards, func(c card.Card, index int) string {
@@ -22,32 +58,27 @@ func main() {
 	var seed int64 = 1665694295623135151
 	randomSource := rand.NewSource(seed)
 	random := rand.New(randomSource)
-	log.Printf("Initialized random with seed %d\n", seed)
+	//cardsInFile := random.Intn(7) + 10 // [10, 17]
+	cards := make([]card.Card, 0)
+	duplicate := make(map[card.Card]int)
 
-	fmt.Println("Starting to generate cards...")
-	for i := 0; i < 100; i++ {
-		log.Printf("Iteration %d\n", i)
-		cardsInFile := random.Intn(7) + 10 // [10, 17]
-		cards := make([]card.Card, 0)
-
-		for j := 0; j < cardsInFile; j++ {
-			generatedCard, _ := card.Random(*random)
+	for len(cards) < 52 {
+		generatedCard, _ := card.Random(*random)
+		if _, found := duplicate[*generatedCard]; !found {
 			cards = append(cards, *generatedCard)
 		}
-		log.Printf("Generated cards %s\n", cards)
-		summary := cardsToRepresentations(cards)
-		file, err := os.Create(fmt.Sprintf("dataset/dat%d.csv", i))
-
-		if err != nil {
-			log.Fatalln("failed to open file", err)
-		}
-
-		writer := csv.NewWriter(file)
-		if err = writer.Write(summary); err != nil {
-			log.Fatalln("error writing to a file!")
-		}
-
-		writer.Flush()
-		_ = file.Close()
+		duplicate[*generatedCard]++
 	}
+	//log.Printf("Generated cards %s\n", cards)
+	generateAll(cards, 0, make([]card.Card, 0), 0)
+	//}
 }
+
+//cards := make([]card.Card, 0)
+//for len(cards) < 52 {
+//generatedCard, _ := card.Random(*random)
+//if _, ok := used[*generatedCard]; !ok {
+//cards = append(cards, *generatedCard)
+//}
+//used[*generatedCard]++
+//}
